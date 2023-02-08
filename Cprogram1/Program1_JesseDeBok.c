249527ca1f1e959d3767b29219318e282bd536ef
Jesse DeBok <jdebok0327@gmail.com>
2023-02-08 14:27:14 -0600

    Finished Program 1 with highest to lowest and 6 month rolling average

--- Cprogram1/Program1_JesseDeBok.c
+++ Cprogram1/Program1_JesseDeBok.c
@@ -37,15 +37,53 @@ void min_max_avg(float sales[12],char* months[12]) {
 	printf("Maximum sales:\t%.2f\t(%s)\n",max,months[whichMax]);
 	printf("Average sales:\t%.2f\n",sum / 12.0);
 }
+void six_month_avg(float sales[12], char* months[12]) {
+	for(int i = 0; i < 7; i++) {
+		float sum = 0;
+		for (int k = i; k < 6 + i; k++) {
+			sum += sales[k];
+		}
+		printf("%s-%s\t%.2f\n",months[i],months[i + 5], sum / 6.0);
+	}
+}
+void print_sales_descending(float sales[12], char* months[12]) {
+	float* sorted_sales = (float*) malloc(sizeof(float) * 12);
+	for(int i = 0; i < 12; i++) {
+		sorted_sales[i] = sales[i];
+	}
+	float temp;
+	for (int i = 0; i < 11; i++) {
+		for (int k = 0; k < 11 - i; k++) {
+			if (sorted_sales[k] > sorted_sales[k+1]) {
+				temp = sorted_sales[k+1];
+				sorted_sales[k+1] = sorted_sales[k];
+				sorted_sales[k] = temp;
+			}
+		}
+	}
+	for (int i = 11; i >= 0; i--) {
+		int whichMonth = 0;
+		for (int k = 0; k < 12; k++) {
+			if (sales[k] == sorted_sales[i]){
+				whichMonth = k;		
+			}
+		}
+		printf("%-10s\t$%.2f\n", months[whichMonth], sorted_sales[i]);
+	}
+}
 int main() {
 	float* month_sales_data = read_data_from("input.txt");
 	char* months[12] = {"January","February","March","April","May","June","July","August","September","October","November","December"};
-	printf("EECS 348 Lab #3 Jesse DeBok, Problem 1\nMonthly Sales Report for 2022\nMonth\t\tSales\n");
+	printf("EECS 348 Lab #3 Jesse DeBok, Problem 1\nMonthly Sales Report for 2022\n\nMonth\t\tSales\n");
 	for(int i = 0; i < 12; i++) {
 		printf("%-10s\t%.2f\n",months[i],month_sales_data[i]);
 	}
-	printf("Sales summary: \n");
+	printf("\nSales summary: \n\n");
 	min_max_avg(month_sales_data, months);
-	
+	printf("\nSix-Month moving average report:\n\n");
+	six_month_avg(month_sales_data, months);
+	printf("\nSales report (highest to lowest):\n\n");
+	printf("Month\t\tSales\n");
+	print_sales_descending(month_sales_data, months);
 	return 0;
 }
